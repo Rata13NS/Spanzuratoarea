131d7d9ba06fc33e6efa6f8e3316ac949b29d3cc
Rata13NS <161605200+Rata13NS@users.noreply.github.com>
2024-03-12 14:13:19 +0200

    Update gallow.js

--- gallow.js
+++ gallow.js
@@ -1,8 +1,8 @@
 let words = ["monitor", "program", "application", "keyboard", "javascript", "gaming", "network"];
-signs = [];
+let signs = [];
 let letterInput = document.getElementById('writeLetter');
 let word = document.getElementById('wordLetters');
-let lives = document.getElementById('gameLifes')
+let lives = document.getElementById('gameLives');
 let wordDisplay = document.getElementById('wordSigns');
 let randomWord;
 let livesContor = 7;
@@ -12,13 +12,13 @@ let signsContor;
 function chooseRandomWord() {
     let randomIndex = Math.floor(Math.random() * words.length);
     randomWord = words[randomIndex];
-    word.innerHTML = 'The word has ' + randomWord.length + ' letters' + '<br>';
+    word.innerHTML = 'The word has ' + randomWord.length + ' letters' + '<br/>';
     for (let i = 0; i < randomWord.length; i++) {
         signs.push('_');
         wordDisplay.innerHTML += signs[i] + ',';
     }
-    wordDisplay.innerHTML += '<br>';
-    lives.innerHTML = 'You have 7 lives ' + '<br>';
+    wordDisplay.innerHTML += '<br/>';
+    lives.innerHTML = 'You have 7 lives ' + '<br/>';
     signsContor = randomWord.length;
 }
 
@@ -44,7 +44,7 @@ function statusGame() {
         wordDisplay.innerHTML = 'You won! Congratulations!!!'
     } else if (letterContor === 0) {
         --livesContor;
-        lives.innerHTML = 'You have ' + livesContor + ' more lifes';
+        lives.innerHTML = 'You have ' + livesContor + ' more lives';
     }
     if (livesContor === 0) {
         word.innerHTML = '';
